87be9ac99a5a34fcb5565a93267cb905619c85eb
nognobi <jenishkhadka9@gmail.com>
2025-02-09 17:06:00 +0545

    jenish

--- practice.js
+++ practice.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+// console.log("Hello World");
+// alert("nigga pigga")
+
+// name = "tony money";
+// console.log(name);
+
+// let name = "dickyson";
+// console.log(name);
+
+// const student ={
+//     fullName : "dickson",
+//     age:20,
+//     cgpa:8.2,
+//     isPass:true,
+// };
+// student.age = student.age +2;   
+
+//product details store
+const productDetails = {
+    fullName: "Parker jotter standard CT Ball Pen ",
+    stars:4,
+    deal:"Deal Of the day",
+    price:270,
+    priceOff:"5% Off",
+};
+console.log(productDetails);
+
+
+const profileInfo = {
+    fullName: "shradha khapra",
+    posts:195,
+    followers:569+"k",
+    following:"4",
+};
+console.log(profileInfo);
